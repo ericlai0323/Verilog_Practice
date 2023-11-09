aef9a98ccde618b34ab3de63a619c88035973d50
LaiYiCheng <laiyichengyuntech@gmail.com>
2023-11-09 22:30:01 +0800

    Update Oscillator

--- IC Design for Communications/HW1_QPSK/RTL/local_oscillator_cos.v
+++ IC Design for Communications/HW1_QPSK/RTL/local_oscillator_cos.v
@@ -1,11 +1,32 @@
 module local_oscillator_cos(
-    input clk_8megahz,
-    input rst_n,
-    input wire signed [13:0] filtered_data,
-    output reg signed [13:0] lo_cos_data
+  input clk_8megahz,
+  input rst_n,
+  input wire signed [13:0] filtered_data, 
+  output reg signed [13:0] lo_cos_data
 );
 
-// output = cos(2*pi*filtered_data/8)
+// 14-bit truncated constants 
+localparam COS_MAX = 14'h2000;   
+// localparam COS_SCALE = 14'h4000;
+
+reg [31:0] phase_accum;
+
+always @(posedge clk_8megahz) begin
+  if(!rst_n) 
+    phase_accum <= 0;
+  else
+    phase_accum <= phase_accum + filtered_data;  
+end
+
+wire [13:0] cos_out;
+
+assign cos_out = COS_MAX - $signed(phase_accum[31:18]);
+
+always @(posedge clk_8megahz) begin
+  if(!rst_n)
+    lo_cos_data <= 0;
+  else 
+    lo_cos_data <= cos_out;
+end
 
 endmodule
-
--- IC Design for Communications/HW1_QPSK/RTL/local_oscillator_nsin.v
+++ IC Design for Communications/HW1_QPSK/RTL/local_oscillator_nsin.v
@@ -1,8 +1,32 @@
 module local_oscillator_nsin(
-    input clk_8megahz,
-    input rst_n,
-    input wire signed [13:0] filtered_data,
-    output reg signed [13:0] lo_nsin_data
+  input clk_8megahz,
+  input rst_n,
+  input wire signed [13:0] filtered_data,
+  output reg signed [13:0] lo_nsin_data  
 );
+
+// 14-bit truncated constants
+localparam SIN_MAX = 14'h2000;
+// localparam SIN_SCALE = 14'h4000; 
+
+reg [31:0] phase_accum;
+
+always @(posedge clk_8megahz) begin
+  if(!rst_n)
+    phase_accum <= 0;
+  else  
+    phase_accum <= phase_accum + filtered_data;
+end
+
+wire [13:0] sin_out;
+
+assign sin_out = SIN_MAX - $signed(phase_accum[31:18]);
+
+always @(posedge clk_8megahz) begin
+  if(!rst_n)
+    lo_nsin_data <= 0;
+  else
+    lo_nsin_data <= -sin_out; // Negate to get negative sine
+end
 
 endmodule
