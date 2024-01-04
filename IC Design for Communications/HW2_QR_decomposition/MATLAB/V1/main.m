fa4361dcfe839f0b37b8c4aa28c2945ab3ed18ca
LaiYiCheng <laiyichengyuntech@gmail.com>
2024-01-04 18:29:06 +0800

    Update MATLAB code

--- IC Design for Communications/HW2_QR_decomposition/MATLAB/V1/main.m
+++ IC Design for Communications/HW2_QR_decomposition/MATLAB/V1/main.m
@@ -3,34 +3,27 @@ fileID = fopen('CSI.txt', 'r');
 data = fscanf(fileID, '%e');
 fclose(fileID);
 
+% 精度為2^8並捨去浮點數，預計誤差2.18%
+for i = 1: 16000
+    data(i) = fix(data(i) * 2^8);
+end
+
 % 將數據整形成 4*4*1000 的三維矩陣
 data = reshape(data, [4, 4, 1000]);
 
 Q_matrix = zeros(4, 4, 1000);
 R_matrix = zeros(4, 4, 1000);
 
-A_matrix = zeros(4, 4, 1000);
-
-% [Q, R] = cordic_qr_decomposition(data(:, :, 1), 16,16);
-% 
-% A = Q * R;
-
 % 循環處理每個4*4矩陣
 for k = 1:1
-
-    [Q, R] = cordic_qr_decomposition(data(:, :, k), 8);
+    
+    [Q, R] = cordic_qr_decomposition(data(:, :, k), 10);
     Q_matrix(:, :, k) = Q;
     R_matrix(:, :, k) = R;
-
-    % A_matrix(:, :, k) = Q*R;
-    % if(A_matrix(:, :, k) == data(:, :, k) )
-    %     disp("A = QR");
-    % else
-    %     disp("A != QR Error!!");
-    % end
-
     
-    % [Q , R] = cordicqr(data(:, :, k));
-    % [Real_Q, Real_R] = qr(data(:, :, k));
+    disp(R);
+    
+    [Cordic_Q , Cordic_R] = cordicqr(data(:, :, k));
+    [Real_Q, Real_R] = qr(data(:, :, k));
 end
 
